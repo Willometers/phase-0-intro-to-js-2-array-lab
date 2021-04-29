7d82e9b83bda2d678b9cff23f55271098af26f1b
BillyMiles <williammiles201@gmail.com>
2021-04-29 10:06:57 -0400

    commit tree abef2b535bee620e076192db60d5589c67c4860d

--- index.js
+++ index.js
@@ -2,33 +2,33 @@
 let cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
-    return cats.push("Ralph")
+    return cats.push(name);
 }
 
 function destructivelyPrependCat(name) {
-    return cats.unshift("Bob")
+    return cats.unshift(name);
 }
 
 function destructivelyRemoveLastCat() {
-    return cats.pop()
+    return cats.pop();
 }
 
 function destructivelyRemoveFirstCat() {
-    return cats.shift()
+    return cats.shift();
 }
 
 function appendCat(name) {
-    return [...cats, name]
+    return [...cats, name];
 }
 
 function prependCat(name) {
-    return [name, ...cats]
+    return [name, ...cats];
 }
 
 function removeLastCat() {
-    return cats.slice(0, 2)
+    return cats.slice(0, 2);
 }
 
 function removeFirstCat() {
-    return cats.slice(1)
+    return cats.slice(-2);
 }
